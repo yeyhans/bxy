93ecbf2d08eef3c04f664e87762494d7b78f578f
yeyhans <yeysonhans@gmail.com>
2026-05-06 12:31:36 -0400

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
+import vercel from '@astrojs/vercel/serverless';
+
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://www.bxy.cl',
+	integrations: [sitemap()],
+	output: 'server',
+	adapter: vercel(),
+});
